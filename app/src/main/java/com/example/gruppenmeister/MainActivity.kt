77eec02f5517456ef5051e64f4e610b42f428420
Melanie Arndt <mar@objektkultur.de>
2023-05-13 20:36:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/gruppenmeister/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.gruppenmeister
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.gruppenmeister.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(Home())
+                R.id.todo -> replaceFragment(Todo())
+                R.id.groups -> replaceFragment(Groups())
+
+                else ->{
+
+
+
+                }
+
+            }
+
+            true
+
+        }
+
+
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
+
+    }
+}
