e8901d3739923f25a6a66ac70a1c33d4647c52d2
Daniel Schleise <102793815+scda1092@users.noreply.github.com>
2023-06-22 22:08:40 +0200

    Update TaskAdapter.kt

--- app/src/main/java/com/example/gruppenmeister/todos/TaskAdapter.kt
+++ app/src/main/java/com/example/gruppenmeister/todos/TaskAdapter.kt
@@ -5,9 +5,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gruppenmeister.databinding.TaskItemCellBinding
 
+// Die Klasse TaskAdapter ist eine RecyclerView.Adapter-Klasse, die TaskViewHolder-Objekte verwendet.
 class TaskAdapter(
-    private val taskItems: List<TaskItem>,
-    private val clickListener: TaskItemClickListener
+    private val taskItems: List<TaskItem>, // Eine Liste von TaskItem-Objekten.
+    private val clickListener: TaskItemClickListener // Ein ClickListener-Objekt.
 ) : RecyclerView.Adapter<TaskViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         val from = LayoutInflater.from(parent.context)
@@ -17,7 +18,8 @@ class TaskAdapter(
 
     override fun getItemCount(): Int = taskItems.size
 
+    // Die onBindViewHolder-Methode wird aufgerufen, wenn ein ViewHolder an eine bestimmte Position gebunden wird.
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        holder.bindTaskItem(taskItems[position])
+        holder.bindTaskItem(taskItems[position]) // Bindet das TaskItem an den ViewHolder.
     }
 }
