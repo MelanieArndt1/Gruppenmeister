259678c5bae1315388a33e0c6ff3ce846d95b29c
Daniel Schleise <102793815+scda1092@users.noreply.github.com>
2023-06-22 22:05:45 +0200

    Update TaskItemClickListener.kt

--- app/src/main/java/com/example/gruppenmeister/todos/TaskItemClickListener.kt
+++ app/src/main/java/com/example/gruppenmeister/todos/TaskItemClickListener.kt
@@ -1,5 +1,6 @@
 package com.example.gruppenmeister.todos
 
+//Interface, für die ClickListener Funktionalitäten
 interface TaskItemClickListener {
     fun showTaskItemDetails (taskItem: TaskItem)
     fun completeTaskItem(taskItem: TaskItem)
