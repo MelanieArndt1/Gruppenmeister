cc71c6480bb4a34e8e8bfaeb3d3f9cf430b8351d
Daniel Schleise <102793815+scda1092@users.noreply.github.com>
2023-06-22 22:24:39 +0200

    Update NewGroupSheet.kt

--- app/src/main/java/com/example/gruppenmeister/groups/NewGroupSheet.kt
+++ app/src/main/java/com/example/gruppenmeister/groups/NewGroupSheet.kt
@@ -10,31 +10,40 @@ import com.example.gruppenmeister.GroupMasterApplication
 import com.example.gruppenmeister.databinding.FragmentNewGroupSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
+// Die Klasse NewGroupSheet erbt von BottomSheetDialogFragment
 class NewGroupSheet(var groupItem: GroupItem? ) : BottomSheetDialogFragment() {
 
+    // Das binding-Objekt wird verwendet, um auf die Ansichten in der Layout-Datei zuzugreifen
     private lateinit var binding: FragmentNewGroupSheetBinding
+
+    // Das groupViewModel-Objekt wird verwendet, um auf die Datenbank zuzugreifen
     private val groupViewModel: GroupViewModel by viewModels {
         val activity= requireActivity()
         GroupItemModelFactory((activity.application as GroupMasterApplication).groupRepository)
     }
+
+    // Die Methode onViewCreated() wird aufgerufen, nachdem die Ansicht erstellt wurde
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Wenn das Feld groupItem nicht null ist, wird der Text des Titels auf "Gruppe bearbeiten" gesetzt und der Name der Gruppe wird in das Textfeld "groupName" eingefügt
         if(groupItem != null) {
             binding.title.text = "Gruppe bearbeiten"
             val editable = Editable.Factory.getInstance()
             binding.groupName.text = editable.newEditable(groupItem!!.groupName)
         }
+        // Andernfalls wird der Titel auf "Neue Gruppe" gesetzt
         else {
             binding.title.text = "Neue Gruppe"
         }
 
-       // groupViewModel = ViewModelProvider(activity).get(GroupViewModel::class.java)
+        // Der OnClickListener für den Button "gruppeSpeichernButton" wird festgelegt
         binding.gruppeSpeichernButton.setOnClickListener{
             saveAction()
         }
     }
 
+    // Die Methode onCreateView() wird aufgerufen, um die Ansicht zu erstellen.
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -43,17 +52,24 @@ class NewGroupSheet(var groupItem: GroupItem? ) : BottomSheetDialogFragment() {
         return binding.root
     }
 
+    // Die Methode saveAction() wird aufgerufen, wenn der Benutzer auf den Button "gruppeSpeichernButton" klickt.
     private fun saveAction() {
+        // Der Name der Gruppe wird aus dem Textfeld "groupName" gelesen
         val groupName = binding.groupName.text.toString()
+
+        // Wenn das Feld groupItem null ist, wird eine neue Gruppe erstellt und zur Datenbank hinzugefügt
         if(groupItem == null)
         {
-            val newGroupItem = GroupItem( groupName)
+            val newGroupItem = GroupItem(groupName)
             groupViewModel.addGroup(newGroupItem)
         }
+        // Andernfalls wird die vorhandene Gruppe aktualisiert
         else {
             groupItem!!.groupName = groupName
             groupViewModel.updateGruppe(groupItem!!)
         }
+
+        // Das Textfeld "groupName" wird geleert und das Fragment geschlossen.
         binding.groupName.setText("")
         dismiss()
     }
